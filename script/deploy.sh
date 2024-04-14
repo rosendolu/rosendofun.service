02c5d103f286cc09f0c41b17e18aebc3fd324801
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-04-14 13:00:48 +0800

    chore:

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 # Check if nvm command is available
 if command -v nvm &>/dev/null; then
     # If nvm command is available, execute nvm use
