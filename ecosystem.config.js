70afcb6d938c31f93caaf4c7d8f810324b402eb9
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-04-14 11:32:15 +0800

    deploy: fix deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'rosendofun.service',
-            watch: ['app', '.env', '.env.local'],
+            // watch: ['app', '.env', '.env.local'],
             // "ignore_watch":['static','script'],
             script: './app/index.js',
         },
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
 nvm use
-unzip -o app.zip -d ./
 npm ci
-pm2 restart rosendofun.service
+
+# Define the name of the PM2 process
+PM2_PROCESS_NAME="rosendofun.service"
+
+# Check if the PM2 process exists
+pm2 pid $PM2_PROCESS_NAME &>/dev/null
+
+if [ $? -eq 0 ]; then
+    # If the process exists, reload it
+    echo "Reloading PM2 process: $PM2_PROCESS_NAME"
+    pm2 reload $PM2_PROCESS_NAME
+else
+    # If the process doesn't exist, start it
+    echo "Starting PM2 process: $PM2_PROCESS_NAME"
+    npm run prod
+fi
