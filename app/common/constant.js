3fcce5c5dd2cb1325373729f886f81e06d121afc
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-04-14 15:55:39 +0800

    chore: secure cookie

--- app/common/constant.js
+++ app/common/constant.js
@@ -6,6 +6,6 @@ module.exports = {
         signed: true /** (boolean) signed or not (default true) */,
         rolling: true /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
         renew: true /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
-        secure: false /** (boolean) secure cookie*/,
+        secure: this.isProdEnv ? true : false /** (boolean) secure cookie*/,
     },
 };
