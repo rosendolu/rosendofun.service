e02b099863100707a1e8fbab10065de140118ef9
rosendo <42633099+rosendolu@users.noreply.github.com>
2024-07-22 10:25:06 +0800

    chore:

--- src/test/binance-spot-balance.js
+++ src/test/binance-spot-balance.js
@@ -2,9 +2,19 @@ const { getLogger } = require('../common/logger');
 const { waitFor, delay } = require('../common/utils');
 const { getCandlesticks, calculateIndicators, matchIndicators, notify } = require('../middleware/binance');
 const binance = require('../service/binance');
-binance.use('spot/candlesticks', getCandlesticks, calculateIndicators, matchIndicators, notify);
+const log = getLogger();
 
-const log = getLogger('binance');
+binance.use('spot/candlesticks', getCandlesticks, calculateIndicators, matchIndicators, (ctx, next) => {
+    let action = '';
+    if (ctx.boll.pb >= 1 && ctx.macd.histogram > 0 && ctx.kdj.J >= 100) {
+        action = 'SELL';
+    }
+    if (ctx.boll.pb <= 0 && ctx.macd.histogram <= 0 && ctx.kdj.J <= 0) {
+        action = 'BUY';
+    }
+    action && log.warn('%s %s %j %j %j', action, ctx.symbol, ctx.boll, ctx.macd, ctx.kdj);
+});
+
 main();
 async function main() {
     try {
@@ -21,7 +31,7 @@ async function main() {
             });
 
             log.info('Binance balance %O', result);
-            waitFor(async () => {
+            await waitFor(async () => {
                 for (const symbol of Object.keys(result)) {
                     log.info('%s %j', symbol, result[symbol]);
                     await binance.trigger('spot/candlesticks', { symbol: symbol + 'USDT', interval: '1d' });
@@ -29,6 +39,21 @@ async function main() {
                 }
                 return 1;
             }, 3e3);
+
+            const iter = [...binance.symbolFilters.keys()];
+            log.debug('Traverse all symbols %s', iter.length);
+
+            if (iter.length === 0) return;
+            await waitFor(async () => {
+                for (const symbol of iter) {
+                    if (!/usdt$/i.test(symbol)) continue;
+                    log.debug('%s', symbol);
+                    await binance.trigger('spot/candlesticks', { symbol: symbol, interval: '1d' });
+                    await delay(300);
+                }
+                return 1;
+            }, 3e3);
+            log.debug('Done!');
         });
     } catch (err) {
         console.error('err:', err);
